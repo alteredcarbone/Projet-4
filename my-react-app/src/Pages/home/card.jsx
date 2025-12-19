232b0c4275b25aa9c1f2a14497ee3a5923ffda59
Maxime Dupont <dupontm698@gmail.com>
2025-12-19 19:18:54 +0100

    creation des card logements

--- my-react-app/src/Pages/home/card.jsx
+++ my-react-app/src/Pages/home/card.jsx
@@ -1,3 +1,27 @@
+import "../../assets/style/components/card.scss";
+import { Link } from "react-router-dom";
+import logements from "../../data/logements.json";
+ 
+
 export default function Card() {
-    return(<h1>Bonjour</h1>)
+    return(
+    <div className="container_card">
+        <section className="gallery">
+            {logements.map((logement) => (
+                <Link
+                    key={logement.id}
+                    to={`/locations/${logement.id}`}
+                    className="card"
+                >
+                    <img 
+                        src={logement.cover}
+                        alt={logement.title}
+                        className="card_img"
+                    />
+                    <h2 className="card_title">{logement.title}</h2>
+                </Link>
+            ))}
+        </section>
+    </div>
+    )
 }
