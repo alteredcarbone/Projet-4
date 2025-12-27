8abaa1af1f9ab64e52cc9daf2c92c3236736deba
alteredcarbone <maximedupont464646@gmail.com>
2025-12-27 23:41:00 +0100

    commencenement about

--- my-react-app/src/Pages/about/about.jsx
+++ my-react-app/src/Pages/about/about.jsx
@@ -1,12 +1,25 @@
 import Banner from "./banner";
-
+import Collapse from "../../Components/collapse/collapse.jsx";
+import "../../assets/style/pages/about.scss";
 
 export default function About() {
   return (
-    <>
-      <Banner />
-     
-      {/* autant de JSX que tu veux */}
-    </>
+    <section className="about">
+    <div className="collapse__about">
+      <Collapse title="Fiabilité" className> 
+        <p>coucou</p>
+      </Collapse>
+      <Collapse title="Respect">
+        <p>Coucou</p>
+      </Collapse>
+      <Collapse title="Service">
+        <p>Coucou</p>
+      </Collapse>
+      <Collapse title="Sécurité">
+        <p>Coucouq</p>
+      </Collapse>
+
+    </div>
+    </section>
   );
 }
